f54d692c35fe7e7d069330383d5670625afa4eb1
Zurisabdai Núñez Velázquez <zurisabdainunezvelazquez@MacBook-Air-de-Zurisabdai.local>
2025-09-08 18:12:24 -0600

    Textos personalizados

---
+++ textos/textos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Textos
+//
+//  Created by Zurisabdai Núñez Velázquez on 08/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
