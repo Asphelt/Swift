3917de0552165f46f32d7ffdf7cef6ff15a09a9c
Zurisabdai Núñez Velázquez <zurisabdainunezvelazquez@MacBook-Air-de-Zurisabdai.local>
2025-09-10 18:58:12 -0600

    Actividad dificultades

---
+++ Seleccion_dificultad/Seleccion_dificultad/Seleccion_dificultadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Seleccion_dificultadApp.swift
+//  Seleccion_dificultad
+//
+//  Created by Zurisabdai Núñez Velázquez on 10/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Seleccion_dificultadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
